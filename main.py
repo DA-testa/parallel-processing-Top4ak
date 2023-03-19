01ad83d0f991c9bef8695417016e30094ec5117f
Top4ak <120311720+Top4ak@users.noreply.github.com>
2023-03-20 00:42:04 +0200

    221RDB014

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 # 221RDB014 Mihails Ruhļa 13. grupa
 
-def parallel_processing(n, m, data):
+def parallel_processing(num_threads, num_jobs, job_times):
     output = []
-    next = [(0, i) for i in range(n)]
-    for work in data:
-        min_index = 0
-        for i in range(1, n):
-            if next[i][0] < next[min_index][0]:
-                min_index = i
-        thread, st = next[min_index]
-        output.append((thread, st))
-        next[min_index] = (st + work, thread)
+    thread_finish_times = [0] * num_threads
+    for job_idx in range(num_jobs):
+        earliest_finish_time = thread_finish_times[0]
+        earliest_finish_thread = 0
+        for thread_idx in range(1, num_threads):
+            if thread_finish_times[thread_idx] < earliest_finish_time:
+                earliest_finish_time = thread_finish_times[thread_idx]
+                earliest_finish_thread = thread_idx
+        output.append((earliest_finish_thread, earliest_finish_time))
+        thread_finish_times[earliest_finish_thread] += job_times[job_idx]
+        
     return output
 
 def main():
